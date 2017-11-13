218d4c4e3119ca9795ff1515859eb43250a410f4
Will Kahn-Greene <willkg@mozilla.com>
2017-11-13 16:36:09 -0500

    Prepare for v1.2 development

--- markus/__init__.py
+++ markus/__init__.py
@@ -17,6 +17,6 @@ __author__ = 'Will Kahn-Greene'
 __email__ = 'willkg@mozilla.com'
 
 # yyyymmdd
-__releasedate__ = '20171113'
+__releasedate__ = ''
 # x.y or x.y.dev0
-__version__ = '1.1'
+__version__ = '1.2.dev0'
