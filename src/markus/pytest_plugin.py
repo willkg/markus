1b39aeb2d4d1f6aeba4ba6506d6b633204ec3c6a
Will Kahn-Greene <willkg@mozilla.com>
2024-06-24 10:49:19 -0400

    Add metrics mock pytest fixture (#118)

--- src/markus/pytest_plugin.py
+++ src/markus/pytest_plugin.py
@@ -2,7 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from markus.testing import MetricsMock
+
 import pytest
 
 
 pytest.register_assert_rewrite("markus.testing")
+
+
+@pytest.fixture
+def metricsmock() -> MetricsMock:
+    with MetricsMock() as mm:
+        yield mm
